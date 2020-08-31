9cda073d4f2fcbf7c5e2a13fc19ec12b58d220f3
name <example.com>
2020-08-31 15:46:01 +0300

    Commit

---
+++ backgrondFon/app.js
@@ -0,0 +1,11 @@
+VANTA.WAVES({
+  el: "#flexBox",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xa0b0c,
+  
+})
